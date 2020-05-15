90f464868c5d1260edeb3b0f9082afc9e1dfb3cc
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-05-15 03:30:18 -0500

    updated readme

--- README.md
+++ README.md
@@ -5,3 +5,16 @@
 ```
 https://download.pytorch.org/models/vgg19-dcbb9e9d.pth
 ```
+
+
+## Orginal Content image
+
+<img src="./images/picaso.jpg"/>
+
+## Original style image 
+
+<img src="./images/dancing.jpeg" />
+
+
+# New Image that my neral style transfer made
+<img src="./images/brycestarry.png" />
--- main.py
+++ main.py
@@ -38,7 +38,7 @@ print(style.shape)
 def im_convert(tensor):
     image = tensor.to("cpu").clone().detach()
     image = image.numpy().squeeze()
-    image = transpose(1, 2, 0)
+    image = image.transpose(1, 2, 0)
     image = image * np.array((0.229, 0.224, 0.225)) + \
         np.array((0.485, 0.456, 0.406))
     image = image.clip(0, 1)
